90a4f92f7d98ad31b563887195d9a9a9bde98758
GopalSaraf <sarafgopal37@gmail.com>
2023-07-30 23:52:42 +0530

    Added setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 import setuptools
 
+import learnML
+
 setuptools.setup(
     include_package_data=True,
     name="learnML",
-    version="0.0.1",
+    version=learnML.__version__,
     description="LearnML - Machine Learning Algorithms",
     url="https://github.com/GopalSaraf/learnML",
     author="Gopal Saraf",
@@ -13,7 +15,9 @@ setuptools.setup(
         "numpy",
         "pandas",
     ],
-    long_description="This package contains the implementation of various machine learning algorithms from scratch using Python 3.6. The algorithms are implemented as classes and are tested on different datasets. The algorithms are provided for educational purposes only and are not intended for production use.",
+    long_description="This package contains the implementation of various machine learning algorithms "
+    "from scratch using Python 3.6. The algorithms are implemented as classes and are tested on different datasets. "
+    "The algorithms are provided for educational purposes only and are not intended for production use.",
     classifiers=[
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
